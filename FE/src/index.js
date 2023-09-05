9b773e28647e8046e61e8c01e3a936265f4a9081
June Hyoung <zxc4554@naver.com>
2023-09-05 13:46:39 +0900

    환경 다시 설정

--- FE/src/index.js
+++ FE/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import store from './redux/store';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import axios from 'axios';
+
+import { persistStore} from 'redux-persist';
+
+let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.withCredentials = true;
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
