6fcb0b6b76e375dd9143bb9723e2971bb6964aaf
June Hyoung <zxc4554@naver.com>
2023-09-11 12:40:01 +0900

    FE-Skeleton

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber'
-import MainPage from './MainPage.js'
+
+
+
+
 
 function App() {
   return (
    <div>
-    <MainPage></MainPage>
+    미완성
    </div>
   );
 }
 
 export default App;
+
+
+
