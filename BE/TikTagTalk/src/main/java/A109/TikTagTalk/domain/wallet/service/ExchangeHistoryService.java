265bf68fa957c76fc87871c528c5580c3b895203
전주영 <bnb3228@naver.com>
2023-09-14 17:42:09 +0900

    feat : exchange dto & service

--- BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/service/ExchangeHistoryService.java
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/service/ExchangeHistoryService.java
@@ -1,4 +1,15 @@
 package A109.TikTagTalk.domain.wallet.service;
 
+import A109.TikTagTalk.domain.wallet.dto.request.ExchangeRequest;
+import A109.TikTagTalk.domain.wallet.dto.response.ExchangeResponse;
+
+import java.time.LocalDateTime;
+
 public interface ExchangeHistoryService {
+    public void test(ExchangeRequest request);
+
+    ExchangeResponse exchangeCoin(LocalDateTime now, Long memberId, int coin, int point, Long exchangeRate);
+
+    public void exchangeAlgo(Long memberId, int amount);
+
 }
--- BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/service/ExchangeHistoryServiceImpl.java
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/service/ExchangeHistoryServiceImpl.java
@@ -1,4 +1,91 @@
 package A109.TikTagTalk.domain.wallet.service;
 
-public class ExchangeHistoryServiceImpl {
+import A109.TikTagTalk.domain.user.entity.Member;
+import A109.TikTagTalk.domain.wallet.dto.request.ExchangeRequest;
+import A109.TikTagTalk.domain.wallet.dto.response.ExchangeResponse;
+import A109.TikTagTalk.domain.wallet.entity.CoinHistory;
+import A109.TikTagTalk.domain.wallet.entity.ExchangeHistory;
+import A109.TikTagTalk.domain.wallet.entity.PointHistory;
+import A109.TikTagTalk.domain.wallet.repository.CoinHistoryRepository;
+import A109.TikTagTalk.domain.wallet.repository.ExchangeHistoryRepository;
+import A109.TikTagTalk.domain.wallet.repository.PointHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ExchangeHistoryServiceImpl implements ExchangeHistoryService{
+    private final ExchangeHistoryRepository exchangeHistoryRepository;
+    private final PointHistoryRepository pointHistoryRepository;
+    private final CoinHistoryRepository coinHistoryRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void test(ExchangeRequest request) {
+        List<ExchangeHistory> list = exchangeHistoryRepository.findAll();
+        return list;
+    }
+
+    @Override
+    public ExchangeResponse exchangeCoin(LocalDateTime now, Long memberId, int coin, int point, Long exchangeRate) throws NullPointerException{
+        //여기서 coin은 환전하려는 코인 갯수
+
+        PointHistory pointHistory1 = pointHistoryRepository.findById(memberId).get();
+
+        Member member = memberRepository.findById(memberId).get();
+
+        Timestamp Tnow = Timestamp.valueOf(now);
+
+        ExchangeRequest exchangeRequest = new ExchangeRequest();
+        exchangeRequest.setExchangeTime(Tnow.toLocalDateTime());
+        exchangeRequest.setCoin(coin);
+        exchangeRequest.setPoint(point);
+        exchangeRequest.setExchangeRate(exchangeRate);
+
+        ExchangeHistory exchangeHistory = exchangeRequest.toEntity();
+
+        exchangeHistoryRepository.save(exchangeHistory);
+
+        CoinHistory coinHistory = new CoinHistory();
+        coinHistory.setCoinTime(Tnow.toLocalDateTime());
+        coinHistory.setCoin(0 + coin);
+        coinHistory.setMember(member);
+
+        List<CoinHistory> coinHistoryList = coinHistoryRepository.findAllByMember_Memberid(member.getId());
+        Integer coins = coinHistoryList.get(coinHistoryList.size() - 1).getBalanceCoin();
+
+        coinHistory.setBalanceCoin(coins);
+
+        coinHistoryRepository.save(coinHistory);
+
+        PointHistory pointHistory = new PointHistory();
+        pointHistory = new PointHistory();
+        pointHistory.setPointTime(Tnow.toLocalDateTime());
+        pointHistory.setPoint(0 - point);
+        pointHistory.setMember(member);
+
+        List<PointHistory> pointHistoryList = pointHistoryRepository.findAllByMember_MemberId(member.getId());
+        Integer points = pointHistoryList.get(pointHistoryList.size()-1).getBalancePoint();
+
+        pointHistory.setBalancePoint(points);
+
+        pointHistoryRepository.save(pointHistory);
+
+
+        ExchangeResponse exchangeResponse = new ExchangeResponse(pointHistory);
+        ExchangeResponse exchangeResponse1 = new ExchangeResponse(coinHistory);
+        ExchangeResponse exchangeResponse2 = new ExchangeResponse(exchangeHistory);
+
+        return null;
+    }
+
+
+    @Override
+    public void exchangeAlgo(Long userId, int amount) {
+
+    }
 }
