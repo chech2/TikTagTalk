0f26cb51fa98a4584a8e161876f922eb3703b380
전주영 <bnb3228@naver.com>
2023-09-18 17:59:14 +0900

    feat : find exchange algo

--- BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/service/ExchangeHistoryServiceImpl.java
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/service/ExchangeHistoryServiceImpl.java
@@ -10,10 +10,18 @@ import A109.TikTagTalk.domain.wallet.repository.CoinHistoryRepository;
 import A109.TikTagTalk.domain.wallet.repository.ExchangeHistoryRepository;
 import A109.TikTagTalk.domain.wallet.repository.PointHistoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +32,9 @@ public class ExchangeHistoryServiceImpl implements ExchangeHistoryService{
     private final PointHistoryRepository pointHistoryRepository;
     private final CoinHistoryRepository coinHistoryRepository;
     private final MemberRepository memberRepository;
+
+    private static HttpURLConnection connection;
+    private static BigDecimal defaultExchangeRate = BigDecimal.valueOf(1300);
 
     @Override
     public void test(ExchangeRequest request) {
@@ -93,6 +104,9 @@ public class ExchangeHistoryServiceImpl implements ExchangeHistoryService{
         //멤버 전체가 보유하고 있는 포인트 개수 p -> member 테이블에 있는 포인트 SUM
         //서버에서 가지고있는 코인 개수 c
 
+
+        //승아 아이디어 : 실제 환율비율로 맞추는 거
+        //api key : KtEmgfK5laxBi7ihB5eq1zR3xiwZG1oO
         Optional<PointHistory> memberPoints = pointHistoryRepository.findById(memberId);
         int totalPoints = 0;
 
@@ -110,5 +124,76 @@ public class ExchangeHistoryServiceImpl implements ExchangeHistoryService{
         int totalCoins = 5000;
         //포인트 개수가 증가할수록 환율 증가
 
+    }
+
+    public static BigDecimal getExchangeRate () {
+
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+        JSONParser parser = new JSONParser();
+
+        String authKey = "KtEmgfK5laxBi7ihB5eq1zR3xiwZG1oO";
+        String searchDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        String dataType = "AP01";
+        BigDecimal exchangeRate = null;
+
+        try {
+            // Request URL
+            URL url = new URL("https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=" + authKey + "&searchdate=" + searchDate + "&data=" + dataType);
+            connection = (HttpURLConnection) url.openConnection();
+
+            // Request 초기 세팅
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+
+            // API 호출
+            // 실패했을 경우 Connection Close
+            if (status > 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else { // 성공했을 경우 환율 정보 추출
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    JSON exchangeRateInfoList = (JSONArray) parser.parse(line);
+
+                    // KRW -> USD에 대한 환율 정보 조회
+                    for (Object o : exchangeRateInfoList) {
+                        JSONObject exchangeRateInfo = (JSONObject) o;
+                        if (exchangeRateInfo.get("cur_unit").equals("USD")) {
+
+                            // 쉼표가 포함되어 String 형태로 들어오는 데이터를 Double로 파싱하기 위한 부분
+                            NumberFormat format = NumberFormat.getInstance(Locale.getDefault());
+                            exchangeRate = new BigDecimal(format.parse(exchangeRateInfo.get("deal_bas_r").toString()).doubleValue());
+                        }
+                    }
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (java.text.ParseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.disconnect();
+        }
+
+        if (exchangeRate == null) {
+            exchangeRate = defaultExchangeRate;
+        }
+
+        return exchangeRate;
     }
 }
