5e675a867e007be0c096846275d6d6c4126b2308
전주영 <bnb3228@naver.com>
2023-09-19 17:51:34 +0900

    feat : currencyAPIService

---
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/controller/CurrencyConverterAPIController.java
@@ -0,0 +1,4 @@
+package A109.TikTagTalk.domain.wallet.controller;
+
+public class CurrencyConverterAPIController {
+}
---
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/dto/ConvertInfoDto.java
@@ -0,0 +1,11 @@
+package A109.TikTagTalk.domain.wallet.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConvertInfoDto {
+    private String receiveCountry;
+    private double sendAmount;
+}
