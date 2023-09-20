56905802d1ace43740f8e27507b0cc4cfdadcfa8
전주영 <bnb3228@naver.com>
2023-09-20 17:52:31 +0900

    feat : currency controller

--- BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/controller/CurrencyConverterAPIController.java
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/controller/CurrencyConverterAPIController.java
@@ -1,4 +1,19 @@
 package A109.TikTagTalk.domain.wallet.controller;
 
+import A109.TikTagTalk.domain.wallet.service.CurrencyConverterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DecimalFormat;
+
+@RestController
+@RequestMapping("/api/exchange/buy")
+@RequiredArgsConstructor
 public class CurrencyConverterAPIController {
+    private final CurrencyConverterService currencyConverter;
+    private final DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+
+    //국가에 따라 환율을 가져오는 메소드
+    @
 }
