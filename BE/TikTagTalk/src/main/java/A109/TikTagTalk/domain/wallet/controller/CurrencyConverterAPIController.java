d786025fa5cb8484044f329c4f98e756262ca861
전주영 <bnb3228@naver.com>
2023-09-21 16:01:21 +0900

    feat : exchange with currency API

--- BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/controller/CurrencyConverterAPIController.java
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/domain/wallet/controller/CurrencyConverterAPIController.java
@@ -1,10 +1,13 @@
 package A109.TikTagTalk.domain.wallet.controller;
 
+import A109.TikTagTalk.domain.wallet.dto.ConvertInfoDto;
 import A109.TikTagTalk.domain.wallet.service.CurrencyConverterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.text.DecimalFormat;
 
 @RestController
@@ -15,5 +18,24 @@ public class CurrencyConverterAPIController {
     private final DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
 
     //국가에 따라 환율을 가져오는 메소드
-    @
+    @GetMapping("/exchagnge-rates")
+    public ResponseEntity getExchangeRage(@RequestParam(name = "receiveCountry") String receiveCountry) {
+        Double exchangeRate = currencyConverter.getCurrencyRate(receiveCountry);
+        return new ResponseEntity(format(exchangeRate), HttpStatus.OK);
+    }
+
+    //환율에 맞게 계산된 금액을 가져오는 메소드(ex, 3코인 설정시 1400*3 포인트)
+    @PostMapping("/exchage-rates")
+    public ResponseEntity getSendAmount(@Valid @RequestBody ConvertInfoDto convertInfoDto){
+
+        Double currency = currencyConverter.getCurrencyRate(convertInfoDto.getReceiveCountry());
+        Double sendAmount = (currency * convertInfoDto.getSendAmount());
+        String formatSendAmount = format(sendAmount);
+
+        return new ResponseEntity(formatSendAmount, HttpStatus.OK);
+    }
+
+    public String format(Number number){
+        return decimalFormat.format(number);
+    }
 }
