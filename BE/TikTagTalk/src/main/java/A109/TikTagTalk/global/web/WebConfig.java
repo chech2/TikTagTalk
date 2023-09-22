700237a1deb9231615237f5e43b7c6c7abd7239c
김선형 <tjsguddl96@gmail.com>
2023-09-22 10:37:57 +0900

    REFACTOR : modify webconfig

--- BE/TikTagTalk/src/main/java/A109/TikTagTalk/global/web/WebConfig.java
+++ BE/TikTagTalk/src/main/java/A109/TikTagTalk/global/web/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS")
-                .allowedHeaders("headers")
+                .allowedHeaders("*")
                 .maxAge(3000);
     }
 }
