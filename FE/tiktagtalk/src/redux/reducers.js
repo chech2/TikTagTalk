c40284b1bfefb44118de72e8d1b788f84082c146
June Hyoung <zxc4554@naver.com>
2023-10-05 14:13:33 +0900

    entire purchase page

--- FE/tiktagtalk/src/redux/reducers.js
+++ FE/tiktagtalk/src/redux/reducers.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 
 import user from './userSlice';
 import item from './itemSlice';
+
 const rootReducer = combineReducers({
   user : user.reducer,
   item : item.reducer,
+
 });
   
 export default rootReducer;
