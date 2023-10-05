7777d6c5912cb38157a36372a8b59e09a04f010f
juuyoungjeon <bnb3228@naver.com>
2023-10-06 06:48:38 +0900

    feat : currency front

--- FE/tiktagtalk/src/pages/ExchangePage.js
+++ FE/tiktagtalk/src/pages/ExchangePage.js
@@ -3,25 +3,41 @@ import './ExchangePage.css'
 
 // import { useState } from "react";
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import Modal from '../components/ui/Modal';
 // import ItemModal from '../components/ItemModal';
-
+import { customAxios } from "../CustomAxios";
 
 
 // 내 소비태그에 변화가 생길 때 마다 axios 요청을 보내서 img url 따와서 박스 만들기
 // 나를 추가한 것에 차이가 생길 때 마다 
 
 function ExchangePage(props) {
+
+    const [Data, setData] = useState();
     let user = useSelector((state) => state.user);
     const [inputValue, setinputValue] = useState('1')
     const handleInputChange = (e) => {
         setinputValue(e.target.value); // 입력 필드의 값 변경 시 상태 업데이트
       };
-    const calculatedValue = parseFloat(inputValue) * 800;
+    const calculatedValue = parseFloat(inputValue) * Data.exchangeRate;
     const handleBuySkin = ()=>{
         console.log()
     }
+
+    useEffect(() => {
+        customAxios
+        .get(process.env.REACT_APP_BASE_URL +"/exchange/buy/exchange-rates?receiveCountry=KRW")
+        .then((response) => {
+            setData(response.data);
+            console.log(response);
+            const data = response.data;
+            console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching contracts:", error);
+        });
+    }, [Data]);
     return (
         <>
             <div>
@@ -58,7 +74,7 @@ function ExchangePage(props) {
             onChange={handleInputChange}
             min="1"/>
             <img className='exchange-responsive-image2' src="./Icon/Coin.png" alt="" />=
-            <div className='point-output'>{isNaN(calculatedValue) ? '' : calculatedValue}</div>
+                <div style={{color:"000000"}} className='point-output'>{isNaN(calculatedValue) ? Data : calculatedValue}</div>
             <img className='exchange-responsive-image2' src="./Icon/포인트 아이콘.png" alt="" />
             <button className='coin-button' onClick={handleBuySkin}> 구매 </button>
         </div>
